e2d27d3a7cded97413b9724f15fc477f49363df4
choikt <choikt123456@gmail.com>
2024-07-11 17:16:48 +0900

    dfdfdfdf

--- modules/route53/main.tf
+++ modules/route53/main.tf
@@ -2,17 +2,17 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_route53_record" "www_record" {
-  zone_id = var.zone_id
-  name    = "www.soldesk-insightforge.site"
-  type    = "A"
+# resource "aws_route53_record" "www_record" {
+#   zone_id = var.zone_id
+#   name    = "www.soldesk-insightforge.site"
+#   type    = "A"
 
-  alias {
-    name                   = var.alb_dns_name
-    zone_id                = var.alb_under_zone_id
-    evaluate_target_health = true
-  }
-}
+#   alias {
+#     name                   = var.alb_dns_name
+#     zone_id                = var.alb_under_zone_id
+#     evaluate_target_health = true
+#   }
+# }
 resource "aws_route53_record" "api_record" {
   zone_id = var.zone_id
   name    = "api.soldesk-insightforge.site"
