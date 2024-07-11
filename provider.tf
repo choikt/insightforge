4fd6eb2a737e3d31cd0ebb77efd977fa6459cc74
choikt <choikt123456@gmail.com>
2024-07-11 16:29:19 +0900

    dfdfdfdf

---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  backend s3 {
+    bucket         = "my-terraform-s3-choi" # S3 버킷 이름
+    key            = "terraform/terraform.tfstate" # tfstate 저장 경로
+    region         = "ap-northeast-2"
+    dynamodb_table = "terraform-tfstate-lock" # dynamodb table 이름
+  }
+}
